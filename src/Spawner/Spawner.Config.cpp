2b464c9694f7a663dd034fd5ae95774651cff7f9
GreyVacuum <2168314900@qq.com>
2026-08-02 23:03:27 +0800

    提交

--- src/Spawner/Spawner.Config.cpp
+++ src/Spawner/Spawner.Config.cpp
@@ -118,6 +118,7 @@ void SpawnerConfig::LoadFromINIFile(CCINIClass* pINI)
 		ContinueWithoutHumans    = pINI->ReadBool(pSettingsSection, "ContinueWithoutHumans", ContinueWithoutHumans);
 		DefeatedBecomesObserver  = pINI->ReadBool(pSettingsSection, "DefeatedBecomesObserver", DefeatedBecomesObserver);
 		Observer_ShowAIOnSidebar = pINI->ReadBool(pSettingsSection, "Observer.ShowAIOnSidebar", Observer_ShowAIOnSidebar);
+		ShowLoadingProgress     = pINI->ReadBool(pSettingsSection, "ShowLoadingProgress", ShowLoadingProgress);
 #ifdef IS_CNCNET_YR_VER
 		DisableChat              = pINI->ReadBool(pSettingsSection, "DisableChat", DisableChat);
 #endif
--- src/Spawner/Spawner.Config.h
+++ src/Spawner/Spawner.Config.h
@@ -147,6 +147,7 @@ public:
 	bool ContinueWithoutHumans;
 	bool DefeatedBecomesObserver;
 	bool Observer_ShowAIOnSidebar;
+	bool ShowLoadingProgress;
 #ifdef IS_CNCNET_YR_VER
 	bool DisableChat;
 #endif
@@ -245,6 +246,7 @@ public:
 		, ContinueWithoutHumans { false }
 		, DefeatedBecomesObserver { false }
 		, Observer_ShowAIOnSidebar { false }
+		, ShowLoadingProgress { true }
 #ifdef IS_CNCNET_YR_VER
 		, DisableChat { false }
 #endif
